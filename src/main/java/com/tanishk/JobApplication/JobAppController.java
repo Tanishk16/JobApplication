89943d9f5fe0cdf721155ac41d2f3c9bf8ecb407
Tanishk Prasad <tanishkprasad8055@gmail.com>
2026-09-06 16:54:39 +0530

    Fixed error and added the controller

---
+++ src/main/java/com/tanishk/JobApplication/JobAppController.java
@@ -0,0 +1,20 @@
+package com.tanishk.JobApplication;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JobAppController {
+
+    @RequestMapping({"/", "home"})
+    public String home(){
+        return "home";
+    }
+
+    @RequestMapping("addjob")
+    public String addJob(){
+        return "addjob";
+    }
+
+
+}
